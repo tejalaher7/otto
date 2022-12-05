44fa603399737612a7d14eeeecd6d8648c2a685a
tejalaher7@gmail.com <tejalaher7@gmail.com>
2022-12-05 18:35:53 +0100

    simple spring boot app fulfilling basic req

---
+++ src/main/java/com/otto/IPRangeController.java
@@ -0,0 +1,38 @@
+package com.otto;
+
+import java.util.List;
+
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.json.simple.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+
+
+//@RequestParam("region") String region
+
+@RestController
+public class IPRangeController {
+	
+	@Autowired
+	private IPRangeService ipRangeService;
+	
+
+	@GetMapping("/ipranges")
+	@Produces({MediaType.TEXT_PLAIN})
+	public @ResponseBody
+    String getipRanges(@RequestParam("region") String region)
+	{
+		return ipRangeService.getIPRanges(region);
+		
+		
+	}
+
+}
---
+++ src/main/java/com/otto/IPRangeService.java
@@ -0,0 +1,103 @@
+package com.otto;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import org.json.simple.JSONObject;
+import org.springframework.stereotype.Service;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+
+
+@Service
+public class IPRangeService {
+	
+	private String awsIPRangeURL = "https://ip-ranges.amazonaws.com/ip-ranges.json";
+	private List<String> validRegions=  Arrays.asList("EU", "US", "AP", "CN", "SA", "AF", "CA");
+	
+	
+
+	public String getIPRanges(String region) {
+		
+		String inline = "";
+		StringBuilder result = new StringBuilder();
+		
+		
+		if(validRegions.contains(region))
+		{
+			try{
+			URL url = new URL("https://ip-ranges.amazonaws.com/ip-ranges.json");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+            int responsecode = conn.getResponseCode();
+            
+
+            if (responsecode != 200) {
+                throw new RuntimeException("HttpResponseCode: " + responsecode);
+            } 
+            
+            else {
+
+                Scanner scanner = new Scanner(url.openStream());
+                //Write all the JSON data into a string using a scanner
+                while (scanner.hasNext()) {
+                    inline += scanner.nextLine();
+                }
+                scanner.close();
+                
+                ObjectMapper mapper = new ObjectMapper();
+                mapper.enable(SerializationFeature.INDENT_OUTPUT);
+                
+                List<JsonNode> prefixNodes = new ArrayList<>();
+
+             // Read the JSON into the Jackson tree model and get the "paths" node
+             JsonNode tree = mapper.readTree(inline);
+             JsonNode prefixes = tree.get("prefixes");            
+             if (prefixes.isArray()) {
+                 ArrayNode arrayNode = (ArrayNode) prefixes;
+                 for (int i = 0; i < arrayNode.size(); i++) 
+                 {
+                     JsonNode individualElement = arrayNode.get(i);                    
+                     if (individualElement.get("region").toString().startsWith("\"" + region.toLowerCase()))
+                     {
+                    	result.append(individualElement);
+                    	result.append("<br>"); 
+                    	 
+                     }
+             
+             
+                 }
+                
+            }
+                
+               
+                
+            }
+    	}
+            catch (Exception e) 
+    		{
+            e.printStackTrace();
+    		}
+		return result.toString();
+		}
+		
+		else
+			return null;
+		
+		
+	}
+
+
+	}
+
+
